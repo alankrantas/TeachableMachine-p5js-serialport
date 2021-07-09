00790c305e2eaf543e76ff9b0e5c3473c71c6d84
Alan Wang <44191076+alankrantas@users.noreply.github.com>
2021-07-09 10:06:13 +0800

    Add files via upload

---
+++ teachable-machine-image-recognition-p5js-serial-port/sketch.js
@@ -0,0 +1,50 @@
+const modelURL = './model/';
+const serialPort = 'COM5';
+
+let classifier;
+let serial;
+let video;
+let flippedVideo;
+let label;
+
+function preload() {
+    classifier = ml5.imageClassifier(modelURL + 'model.json');
+    serial = new p5.SerialPort();
+}
+
+function setup() {
+    serial.open(serialPort);
+    createCanvas(320, 260);
+    video = createCapture(VIDEO);
+    video.size(320, 240);
+    video.hide();
+    flippedVideo = ml5.flipImage(video);
+    classifyVideo();
+}
+
+function draw() {
+    background(0);
+    image(flippedVideo, 0, 0);
+    fill(255);
+    textSize(16);
+    textAlign(CENTER);
+    text(label, width / 2, height - 4);
+}
+
+function classifyVideo() {
+    flippedVideo = ml5.flipImage(video)
+    classifier.classify(flippedVideo, gotResult);
+    flippedVideo.remove();
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    label = String(results[0].label);
+    console.log(label);
+    serial.write(label + '\r\n');
+    classifyVideo();
+}
+
